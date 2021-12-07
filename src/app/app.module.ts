d1fa2aa27debf6204b55c61752509bcc2aa656ef
Isabelle <isabelle.tieu@gmail.com>
2021-12-07 10:07:26 +0100

    Still trying to get the values from the radio buttons form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, Page1Component],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -8,6 +8,11 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./page1.component.css'],
 })
 export class Page1Component implements OnInit {
+  public FrenchRegions!: string;
+  FrenchDepartments!: string;
+  FrenchCommunes!: string;
+  radioValue = {};
+
   constructor(private http: HttpClient) {
     this.http
       .get(`geo.api.gouv.fr/regions`)
@@ -15,6 +20,10 @@ export class Page1Component implements OnInit {
       .then(function (res) {
         console.log(res);
       });
+  }
+
+  submittedForm() {
+    console.log('Form submitted');
   }
 
   ngOnInit(): void {}
